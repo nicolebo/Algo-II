8cb85a8c7752aedf570184eedcc8699f4585141b
nicolas <nicolaslebovits@gmail.com>
2018-04-05 21:29:52 -0500

    terminado de la pila

--- pila/pila.c
+++ pila/pila.c
@@ -1,5 +1,6 @@
 #include "pila.h"
 #include <stdlib.h>
+#define CAPACIDAD 4;
 
 /* Definición del struct pila proporcionado por la cátedra.
  */
@@ -9,8 +10,81 @@ struct pila {
     size_t capacidad;  // Capacidad del arreglo 'datos'.
 };
 
-/* *****************************************************************
- *                    PRIMITIVAS DE LA PILA
- * *****************************************************************/
+// Crea una pila.
+// Post: devuelve una nueva pila vacía.
+pila_t* pila_crear(void)
+{
+    pila_t* pila = malloc(sizeof(pila_t));
+    if (pila == NULL) return NULL;
+    pila->cantidad = 0;
+    pila->capacidad = CAPACIDAD;
+    pila->datos = malloc(pila->cantidad * sizeof(void));
+    return pila;
+}
 
-// ...
+// cambia la dimension de una pila
+//Post: devuelve true si pudo cambiar el tamano sin errores
+bool cambiar_dimension(pila_t *pila, size_t tam)
+{
+    void *nuevos_datos = realloc(pila->datos, tam * sizeof(void));
+    if (nuevos_datos == NULL) return false;
+    pila->datos = nuevos_datos;
+    pila->capacidad = tam;
+    return true;
+}
+
+void pila_destruir(pila_t *pila)
+{
+    free(pila->datos);
+    free(pila);
+}
+
+bool pila_esta_vacia(const pila_t *pila)
+{
+    return pila->cantidad == 0 ? true : false;
+}
+
+// Agrega un nuevo elemento a la pila. Devuelve falso en caso de error.
+// Pre: la pila fue creada.
+// Post: se agregó un nuevo elemento a la pila, valor es el nuevo tope.
+bool pila_apilar(pila_t *pila, void* valor)
+{
+    if (pila->capacidad > pila->cantidad)
+    {
+        if (!cambiar_dimension(pila, pila->capacidad * 2)) return false;
+    }
+
+    pila->datos[pila->cantidad] = valor;
+    pila->cantidad =+ 1;
+    return true;
+
+}
+
+// Obtiene el valor del tope de la pila. Si la pila tiene elementos,
+// se devuelve el valor del tope. Si está vacía devuelve NULL.
+// Pre: la pila fue creada.
+// Post: se devolvió el valor del tope de la pila, cuando la pila no está
+// vacía, NULL en caso contrario.
+void* pila_ver_tope(const pila_t *pila)
+{
+    if (pila_esta_vacia(pila)) return NULL;
+    return pila->datos[pila->cantidad-1];
+}
+
+// Saca el elemento tope de la pila. Si la pila tiene elementos, se quita el
+// tope de la pila, y se devuelve ese valor. Si la pila está vacía, devuelve
+// NULL.
+// Pre: la pila fue creada.
+// Post: si la pila no estaba vacía, se devuelve el valor del tope anterior
+// y la pila contiene un elemento menos.
+void* pila_desapilar(pila_t *pila)
+{
+    if (pila_esta_vacia(pila)) return NULL;
+    if (pila->cantidad <= pila->capacidad/4)
+    {
+        if(!cambiar_dimension(pila, pila->capacidad/2)) return NULL;
+    }
+
+    pila->cantidad -= 1;
+    return pila->datos[pila->cantidad];
+}
--- pila/pruebas_alumno.c
+++ pila/pruebas_alumno.c
@@ -7,8 +7,104 @@
  *                   PRUEBAS UNITARIAS ALUMNO
  * *****************************************************************/
 
+void prueba_crear_pila()
+{
+    pila_t* pila = pila_crear();
+    print_test("Creacion de la pila", pila == NULL ? false : true);
+    pila_destruir(pila);
+
+}
+void prueba_apilar()
+{
+    int variable = 1;
+    pila_t* pila = pila_crear();
+    pila_apilar(pila, &variable);
+    print_test("Apilar un elemento", pila_esta_vacia(pila) ? false: true);
+    pila_destruir(pila);
+
+}
+void prueba_desapilar()
+{
+    int variable = 1;
+    pila_t* pila = pila_crear();
+    pila_apilar(pila, &variable);
+    pila_desapilar(pila);
+    print_test("desapilar un elemento", pila_esta_vacia(pila) ? true : false);
+    pila_destruir(pila);
+}
+void prueba_apilo_y_desapilo_x_elementos()
+{
+    int elementos = 100;
+    int variable = 87;
+    pila_t* pila = pila_crear();
+
+    for (int i = 0; i < elementos; ++i) {
+        pila_apilar(pila, &variable);
+    }
+    print_test("se apilaron correctacmente?", pila_esta_vacia(pila) ? false : true);
+
+    for (int j = 0; j < elementos; ++j) {
+        pila_desapilar(pila);
+    }
+    print_test("se desapilaron correctacmente?", pila_esta_vacia(pila) ? true : false);
+
+    pila_destruir(pila);
+
+}
+
+void prueba_apilo_elemento_nulo()
+{
+    pila_t* pila = pila_crear();
+    pila_apilar(pila, NULL);
+    print_test("El tope deberia ser null", pila_ver_tope(pila) == NULL ? true : false);
+    pila_destruir(pila);
+
+
+}
+void pruebo_desapilar_en_pila_vacia()
+{
+    pila_t* pila = pila_crear();
+    print_test("la pila estaba vacia", pila_desapilar(pila) == NULL ? true : false);
+    pila_destruir(pila);
+
+
+}
+void pruebo_vet_topo_en_pila_vacia()
+{
+    pila_t* pila = pila_crear();
+    print_test("tope de pila estaba vacia", pila_ver_tope(pila) == NULL ? true : false);
+    pila_destruir(pila);
+}
+void pruebo_pila_creada_esta_vacia()
+{
+    pila_t* pila = pila_crear();
+    print_test("pila esta vacia", pila_esta_vacia(pila) ? true : false);
+    pila_destruir(pila);
+}
+
+
+void pruebas_caso_apilar_y_desapilar_para_ver_pila()
+{
+    int variable = 1;
+    pila_t* pila = pila_crear();
+    pila_apilar(pila, &variable);
+    pila_desapilar(pila);
+    print_test("desapilar", pila_desapilar(pila) == NULL ? true : false);
+    print_test("ver tope", pila_ver_tope(pila) == NULL ? true : false);
+    pila_destruir(pila);
+}
+
 void pruebas_pila_alumno() {
     pila_t* ejemplo = NULL;
 
     print_test("Puntero inicializado a NULL", ejemplo == NULL);
+    prueba_crear_pila();
+    prueba_apilar();
+    prueba_desapilar();
+    prueba_apilo_elemento_nulo();
+    prueba_apilo_y_desapilo_x_elementos();
+    pruebo_desapilar_en_pila_vacia();
+    pruebo_vet_topo_en_pila_vacia();
+    pruebo_pila_creada_esta_vacia();
+    pruebas_caso_apilar_y_desapilar_para_ver_pila();
 }
